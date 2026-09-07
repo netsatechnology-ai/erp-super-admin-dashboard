93a7eee0c6271539e3f95dc7eda681987bdfe889
tilahun <totilahunfeyissa@gmail.com>
2026-09-07 16:40:35 +0300

    build fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // Forces a static HTML/CSS/JS export
+  images: {
+    unoptimized: true, // Required because cPanel won't have Next.js's image optimization engine
+  },
 };
 
 export default nextConfig;
